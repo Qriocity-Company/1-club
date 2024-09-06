8a91ba4679216bb50d9a95fd71a39d6c517560e4
SOMESHTHAPLIYALL <149586000+Qriocity-Company@users.noreply.github.com>
2024-09-06 21:08:31 +0530

    update

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -5,7 +5,7 @@ import Carasoule from "./carasoule";
 
 function Testimonials() {
   return (
-    <div className="w-full font-serif  bg-[#e3efec] flex  justify-center items-center px-2 md:px-0 py-10 md:py-20">
+    <div className="w-full font-serif   flex  justify-center items-center px-2 md:px-0 py-10 md:py-20">
       <div className="w-full   flex flex-col justify-center items-center gap-6 md:gap-16">
         <div className="bg-black flex items-center gap-4 pl-2 pr-8 rounded-full py-2">
           <div className="flex p-2 justify-center items-center rounded-full bg-[#C4F72C]">

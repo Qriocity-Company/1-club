be19fb50a770b773405d4ba4f35be17f1cd0d2be
dev-9820 <dev.bhanushali03@gmail.com>
2024-11-12 01:30:55 +0530

    Commit

--- src/Components/landingPage/Testimonials.js
+++ src/Components/landingPage/Testimonials.js
@@ -7,7 +7,7 @@ function Testimonials() {
     <div className="w-full font-sans  flex  justify-center items-center px-2 md:px-0 py-10 md:py-20">
       <div className="w-full flex flex-col justify-center items-center  ">
         <h1 className=" text-center text-[32px] text-black md:text-[54px] font-semibold ">
-        Don’t believe in my words !
+        Don’t believe in my words
         </h1>
         <h2 className=" text-center text-[32px] text-black md:text-[50px] font-semibold" >See the Success Stories here</h2>
         <div>
--- src/Pages/LandingPageTwo.js
+++ src/Pages/LandingPageTwo.js
@@ -30,7 +30,7 @@ import leaf4 from "../assets/leaf4.png";
 import leaf5 from "../assets/leaf5.png";
 import icon from "../assets/icon.png";
 import arrowright from "../assets/arrowright.png"
-
+import ChatSlider from "../Components/landingPage/ChatSlider"
 
 const LandingPageTwo = () => {
 
@@ -183,7 +183,7 @@ const LandingPageTwo = () => {
             
             className="bg-[#0B6544] my-5 hover:bg-green-900 transition-all  duration-300 rounded-full px-4 py-2 lg:px-6 lg:py-4 text-white text-[24px] lg:text-[32px] font-medium"
           >
-            Remove these thoughts From You
+            REMOVE THESE THOUGHTS FROM YOU
           </button>
           </Link>
           </div>
@@ -206,7 +206,7 @@ const LandingPageTwo = () => {
             
             className="bg-[#0B6544] mb-5 rounded-full px-4 py-2 lg:px-6 lg:py-4 text-white text-[24px] lg:text-[32px] mt-5 font-medium"
           >
-            Book Your MAP Session Now
+            BOOK YOUR MAP SESSION NOW
           </button>
           </Link>
           
@@ -442,12 +442,13 @@ const LandingPageTwo = () => {
             
             className="bg-[#0B6544] mb-5 rounded-full px-4 py-2 lg:px-6 lg:py-4 text-white text-[24px] lg:text-[32px] font-medium"
           >
-            BOOK YOUR CALL
+            BOOK YOUR CALL NOW
           </button></Link>
             </div>
 
         
         <Testimonials/>
+        <ChatSlider/>
        
       </div>
     </>

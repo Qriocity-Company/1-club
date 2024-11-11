afca57173bbc111e6b1a2d156e79a421f7294716
dev-9820 <dev.bhanushali03@gmail.com>
2024-11-12 01:53:44 +0530

    Update ImageCarousel.js

--- src/Components/landingPage/ImageCarousel.js
+++ src/Components/landingPage/ImageCarousel.js
@@ -4,18 +4,18 @@ import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 const ImageCarousel = ({ screenshots }) => {
   const carouselRef = useRef(null);
 
-  // Scroll the carousel to the left
+  // Scroll left function
   const scrollLeft = () => {
     carouselRef.current.scrollBy({
-      left: -500,
+      left: -300,
       behavior: 'smooth',
     });
   };
 
-  // Scroll the carousel to the right
+  // Scroll right function
   const scrollRight = () => {
     carouselRef.current.scrollBy({
-      left: 500,
+      left: 300,
       behavior: 'smooth',
     });
   };
@@ -25,22 +25,25 @@ const ImageCarousel = ({ screenshots }) => {
       {/* Left Arrow Button */}
       <button
         onClick={scrollLeft}
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-green-500 text-white p-2 rounded-full shadow-md hover:bg-green-600 z-10"
+        className="absolute left-2 md:left-4 top-1/2 transform -translate-y-1/2 bg-green-500 text-white p-1 md:p-2 rounded-full shadow-md hover:bg-green-600 z-10"
       >
-        <FiChevronLeft size={20} />
+        <FiChevronLeft className="w-6 h-6 md:w-8 md:h-8" />
       </button>
 
       {/* Carousel Container */}
       <div
         ref={carouselRef}
-        className="flex overflow-x-scroll gap-6 p-4 scroll-smooth hide-scrollbar"
+        className="flex overflow-x-scroll gap-4 p-4 scroll-smooth hide-scrollbar"
       >
         {screenshots.map((image, index) => (
-          <div key={index} className="min-w-[500px] flex-shrink-0">
+          <div
+            key={index}
+            className="flex-shrink-0 w-[80vw] sm:w-[60vw] md:w-[45vw] lg:w-[30vw] xl:w-[25vw]"
+          >
             <img
               src={image}
               alt={`Screenshot ${index + 1}`}
-              className="w-full h-[400px] object-cover rounded-xl shadow-lg"
+              className="w-full h-[200px] sm:h-[250px] md:h-[300px] lg:h-[350px] object-cover rounded-xl shadow-lg"
             />
           </div>
         ))}
@@ -49,9 +52,9 @@ const ImageCarousel = ({ screenshots }) => {
       {/* Right Arrow Button */}
       <button
         onClick={scrollRight}
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-green-500 text-white p-2 rounded-full shadow-md hover:bg-green-600 z-10"
+        className="absolute right-2 md:right-4 top-1/2 transform -translate-y-1/2 bg-green-500 text-white p-1 md:p-2 rounded-full shadow-md hover:bg-green-600 z-10"
       >
-        <FiChevronRight size={20} />
+        <FiChevronRight className="w-6 h-6 md:w-8 md:h-8" />
       </button>
     </div>
   );

f7c11be00c859f2ca47a39aabf7f5c18d5be9034
dev <149586000+Qriocity-Company@users.noreply.github.com>
2025-01-09 15:32:06 +0530

    Update IntroductionTwo.js

--- src/Components/landingPage/IntroductionTwo.js
+++ src/Components/landingPage/IntroductionTwo.js
@@ -14,7 +14,7 @@ const IntroductionTwo = () => {
     //   });
     // }
     
-    window.open("https://app.simplymeet.me/trauma2triumph/trauma2triumph-15-1731481629597", "_blank");
+    window.open("hhttps://app.simplymeet.me/trauma2triumph/trauma2triumph", "_blank");
   };
 
   return (
